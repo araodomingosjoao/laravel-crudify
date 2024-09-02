40d783840de5c5c0eedf113682686ef6c42fb9d4
Arao Domingos <araodomingos04@gmail.com>
2024-09-02 07:15:49 +0100

    refactor: melhorias e ajustes do pacote crudify

---
+++ config/crudify.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'model_namespace' => 'App\\Models\\',
+    'controller_namespace' => 'App\\Http\\Controllers\\',
+    'request_namespace' => 'App\\Http\\Requests\\',
+    'use_soft_deletes' => false,
+];
